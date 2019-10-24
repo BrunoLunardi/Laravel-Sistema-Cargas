727c01ac9a85d3d0781f8eba1ae96cfc9185bd9c
bruno <nardi273@gmail.com>
2019-10-23 21:44:59 -0300

    Inicio Implementação OpenStreetMaps

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,17 @@
 @extends('adminlte::page')
+{{-- CSS para interação com maps --}}
+ <link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css"
+   integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+   crossorigin=""/>
+
+ <!-- Make sure you put this AFTER Leaflet's CSS -->
+ <script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js"
+   integrity="sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og=="
+   crossorigin=""></script>
+
+
+{{-- css maps --}}
+@mapstyles
 
 @section('title', 'AdminLTE')
 
@@ -7,5 +20,49 @@
 @stop
 
 @section('content')
-    <p>You are logged in!</p>
+<div id="mapid">
+@map([
+    'lat' => '48.134664',
+    'lng' => '11.555220',
+    'zoom' => '6',
+        'markers' => [[
+        'title' => 'Go NoWare',
+        'lat' => '48.134664',
+        'lng' => '11.555220',
+    ]],
+])
+
+
+</div>
+
+<script>
+    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	id: 'mapbox.streets',
+	accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(mymap);
+}
+
+// mymap.on('click', onMapClick);
+
+function onMapClick(e) {
+	alert("You clicked the map at " + e.latlng);
+}
+
+mymap.on('click', onMapClick);
+
+</script>
+    {{-- scrips maps --}}
+    @mapscripts
 @stop
